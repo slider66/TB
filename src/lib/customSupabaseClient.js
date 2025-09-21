538312f4e033012dcf15934b3643c735848de075
Alejandro Merle <alexmerle66@gmail.com>
2025-09-21 19:07:43 +0200

    chore: update dependencies and refactor Supabase client configuration

--- src/lib/customSupabaseClient.js
+++ src/lib/customSupabaseClient.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ayafzwwklyjbzrehwukf.supabase.co'; // URL correcta
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5YWZ6d3drbHlqYnJ6ZWh3dWtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY5MjYwOTcsImV4cCI6MjA3MjUwMjA5N30.LzuoVX8Fasf_Qbz0U9RHiohEnZ_PBuK5-7focR2_9_Q';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
