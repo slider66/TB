8c9cf6cafb0e679598af2493682301c50bb86614
Alejandro Merle <alexmerle66@gmail.com>
2025-10-10 20:18:16 +0200

    chore: test

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,12 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']
+    ]
+  }
 };
